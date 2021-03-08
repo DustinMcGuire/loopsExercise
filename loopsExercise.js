33740d4291d92b61c88c7a88a7ea70011a86061a
Dustin McGuire <dustinmcguire@KCCCs-MBP-3.lan1>
2021-03-08 10:06:19 -0500

    test commit

---
+++ loopsExercise.js
@@ -0,0 +1,24 @@
+// 1
+for (i = 1; i <= 7; i++){
+    console.log(i);
+  }
+// 2
+for (i = 5; i < 29; i+=4){
+    console.log(i);
+}
+// 3a
+const wizards = [
+    'Harry Potter',
+    'Hermione Granger',
+    'Ron Weasley'
+];
+// 3b
+for (item of wizards){
+    console.log(item);
+}
+// 4a
+let harryPotterMovies = 0
+while (harryPotterMovies <= 8){
+    console.log(harryPotterMovies);
+    harryPotterMovies++;
+  }
